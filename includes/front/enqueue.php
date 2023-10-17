5c2b89243266d1a04e596d6bb0bbe4c2713b06e7
Bergan Oudshoorn <bergan404@gmail.com>
2023-10-17 13:14:43 -0400

    finally added the splide to the enqueue file so that it is easier to make sliders

--- includes/front/enqueue.php
+++ includes/front/enqueue.php
@@ -12,21 +12,26 @@ function custom_theme_enqueue()
 
     wp_register_style('bootstrap5_style', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css', [], $ver);
 
+    wp_register_style('splide_style', 'https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css', [], $ver);
+
     wp_register_style('custom_theme_style', get_stylesheet_uri(), [], $ver);
 
     wp_enqueue_style('aos_style');
     wp_enqueue_style('font_awesome');
     wp_enqueue_style('bootstrap5_style');
+    wp_enqueue_style('splide_style');
     wp_enqueue_style('custom_theme_style');
 
     wp_register_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js', [], $ver);
     wp_register_script('aos_script', 'https://unpkg.com/aos@2.3.1/dist/aos.js', [], $ver, true);
     wp_register_script('bootstrap5_script', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js', $ver, true);
+    wp_register_script('splide_script', 'https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js', $ver, true);
     wp_register_script('custom_theme_script', $uri . '/assets/js/bundle.js', [], $ver, true);
 
 
     wp_enqueue_script("jquery");
     wp_enqueue_script('aos_script');
     wp_enqueue_script('bootstrap5_script');
+    wp_enqueue_script('splide_script');
     wp_enqueue_script('custom_theme_script');
 }
---
+++ src/js/slider.js
@@ -0,0 +1,10 @@
+var splide = new Splide( '.splide', {
+	type: 'loop',
+	perPage: 4,
+	perMove: 1,
+	arrows: true,
+	autoplay: true,
+	interval: 7000
+});
+
+splide.mount();
